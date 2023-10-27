0571ec7abd7a7f09910cc5fce9d4ddb7fe8136b0
Rony015 <53683822+Rony015@users.noreply.github.com>
2023-10-27 17:32:24 +0600

    admin dashboard

--- includes/Admin/templates/dashboard.php
+++ includes/Admin/templates/dashboard.php
@@ -2,16 +2,61 @@
     <div class="spe_dashboard_banner">
         <img src="<?php echo SPE_IMG ?>/dashboard-img/spe-log.png; ?>" alt="spe-logo">
     </div>
-    <div class="spe-grid spe-grid-cols-12">
-        <div class="spe-lg-col-3">
-            <div class="spe_widget_progress_item">
+    <div class="ezd-grid ezd-grid-cols-12">
+        <div class="ezd-lg-col-3">
+            <div class="spe_widget_progress_item ezd-text-center">
                 <h3>All Widget</h3>
                 <svg class="radial-progress" data-percentage="85" viewBox="0 0 80 80">
                     <circle class="incomplete" cx="40" cy="40" r="35"></circle>
                     <circle class="complete" cx="40" cy="40" r="35"></circle>
-                    <text class="percentage" x="50%" y="57%" transform="matrix(0, 1, -1, 0, 80, 0)">85%</text>
+                    <text class="percentage" x="-48%" y="59%">85%</text>
                 </svg>
-                <ul class="spe-list-unstyled">
+                <ul class="ezd-list-unstyled progress_info_list">
+                    <li>Total : 250</li>
+                    <li>Used : 155</li>
+                    <li>Unused : 95</li>
+                </ul>
+            </div>
+        </div>
+        <div class="ezd-lg-col-3">
+            <div class="spe_widget_progress_item ezd-text-center">
+                <h3>Core</h3>
+                <svg class="radial-progress" data-percentage="65" viewBox="0 0 80 80">
+                    <circle class="incomplete" cx="40" cy="40" r="35"></circle>
+                    <circle class="complete" cx="40" cy="40" r="35"></circle>
+                    <text class="percentage" x="-48%" y="59%">65%</text>
+                </svg>
+                <ul class="ezd-list-unstyled progress_info_list">
+                    <li>Total : 250</li>
+                    <li>Used : 155</li>
+                    <li>Unused : 95</li>
+                </ul>
+            </div>
+        </div>
+        <div class="ezd-lg-col-3">
+            <div class="spe_widget_progress_item ezd-text-center">
+                <h3>3rd Party</h3>
+                <svg class="radial-progress" data-percentage="62" viewBox="0 0 80 80">
+                    <circle class="incomplete" cx="40" cy="40" r="35"></circle>
+                    <circle class="complete" cx="40" cy="40" r="35"></circle>
+                    <text class="percentage" x="-48%" y="59%">62%</text>
+                </svg>
+                <ul class="ezd-list-unstyled progress_info_list">
+                    <li>Total : 250</li>
+                    <li>Used : 155</li>
+                    <li>Unused : 95</li>
+                </ul>
+            </div>
+        </div>
+        <div class="ezd-lg-col-3">
+            <div class="spe_widget_progress_item ezd-text-center">
+                <h3>Active</h3>
+                <svg class="radial-progress" data-percentage="65" viewBox="0 0 80 80">
+                    <circle class="incomplete" cx="40" cy="40" r="35"></circle>
+                    <circle class="complete" cx="40" cy="40" r="35"></circle>
+                    <text class="percentage" x="-48%" y="59%">65%</text>
+                </svg>
+                <ul class="ezd-list-unstyled progress_info_list">
                     <li>Total : 250</li>
                     <li>Used : 155</li>
                     <li>Unused : 95</li>
@@ -19,20 +64,77 @@
             </div>
         </div>
     </div>
-    <div class="spe-grid spe-grid-cols-12">
-        <div class="spe-lg-col-6">
+    <div class="ezd-grid ezd-grid-cols-12">
+        <div class="ezd-lg-col-6">
             <div class="spe_support_item">
                 <span class="spe_icon icon-documentation"></span>
-                <h2>Documentation</h2>
+                <h2 class="spe_dashboard_title">Documentation</h2>
                 <p>Get detailed and guided instruction to level up your website with the necessary set up.</p>
                 <a href="#" class="spe_dashboard_btn">Check Documentation</a>
             </div>
         </div>
-        <div class="spe-lg-col-6">
+        <div class="ezd-lg-col-6">
             <div class="spe_support_item">
                 <span class="spe_icon icon-help"></span>
-                <h2>Need Help</h2>
+                <h2 class="spe_dashboard_title">Need Help</h2>
                 <p>If you are stuck at anything while using our product, reach out to us immidieatly</p>
+                <a href="#" class="spe_dashboard_btn">Support Ticket</a>
+            </div>
+        </div>
+    </div>
+    <div class="ezd-grid ezd-grid-cols-12">
+        <div class="ezd-lg-col-12">
+            <div class="spe_support_item">
+                <h2 class="spe_dashboard_title">System Requirement</h2>
+                <div class="ezd-grid ezd-grid-cols-12">
+                    <ul class="spe-list-unstyled spe_requirment_list ezd-lg-col-6">
+                        <li><strong>Memory Limit</strong> <span><i class="icon-check"></i>Currently 8.1.9</span></li>
+                        <li><strong>PHP Version</strong> <span><i class="icon-cross"></i>Currently 256M</span></li>
+                        <li><strong>Folder writable</strong> <span><i class="icon-check"></i>Currently 8.1.1</span></li>
+                        <li><strong>GZip Enabled:</strong> <span><i class="icon-check"></i>Currently 256M</span></li>
+                    </ul>
+                    <ul class="spe-list-unstyled spe_requirment_list ezd-lg-col-6">
+                        <li><strong>Max execution time:</strong> <span><i class="icon-check"></i>Currently 1400</span>
+                        </li>
+                        <li><strong>Max post limit:</strong> <span><i class="icon-cross"></i>Currently 1000M</span></li>
+                        <li><strong>Multisite:</strong> <span><i class="icon-check"></i>Currently 8.1.9</span></li>
+                        <li><strong>Debug Mode:</strong> <span><i class="icon-check"></i>Currently 8.1.9</span></li>
+                    </ul>
+                </div>
+                <div class="note">
+                    <p>Note: If you have multiple addons like Element Pack so you need some more requirement some cases
+                        so make sure you added more memory for others addon too.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ezd-grid ezd-grid-cols-12">
+        <div class="ezd-lg-col-6">
+            <div class="spe_support_item">
+                <span class="spe_icon icon-features"></span>
+                <h2 class="spe_dashboard_title">Missing Features</h2>
+                <p>Send us a message if you believe the plugin is<br> lacking any features.</p>
+                <a href="#" class="spe_dashboard_btn">Request Features</a>
+            </div>
+        </div>
+        <div class="ezd-lg-col-6">
+            <div class="spe_support_item dashboard_img">
+                <img src="<?php echo SPE_IMG ?>/dashboard-img/table_work.png; ?>" alt="work">
+            </div>
+        </div>
+        <div class="ezd-lg-col-6">
+            <div class="spe_support_item">
+                <span class="spe_icon icon-love"></span>
+                <h2 class="spe_dashboard_title">Show Your Love</h2>
+                <p>Leave your feedback to help us out if you liked<br> our product and customer service.</p>
+                <a href="#" class="spe_dashboard_btn">Check Documentation</a>
+            </div>
+        </div>
+        <div class="ezd-lg-col-6">
+            <div class="spe_support_item">
+                <span class="spe_icon icon-debug"></span>
+                <h2 class="spe_dashboard_title">Facing an issues?</h2>
+                <p>You think there is a bug in the product? Inform<br> us please!</p>
                 <a href="#" class="spe_dashboard_btn">Support Ticket</a>
             </div>
         </div>
