b87b08f3edff19ac4cde74021942c486fb5b3ab6
Arif Rahman <arifbb79@gmail.com>
2023-11-13 19:23:31 +0600

    update: testimonials widgets

--- widgets/templates/testimonials/testimonial-7.php
+++ widgets/templates/testimonials/testimonial-7.php
@@ -5,16 +5,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="feedback-section-five">
     <div class="feedback-slider-one testimonial-slider-<?php echo esc_attr( $testimonial_id ); ?>">
-        <?php if ( ! empty( $testimonials6 ) ) {
+		<?php if ( ! empty( $testimonials6 ) ) {
 			foreach ( $testimonials6 as $item ) {
 				$rating_data    = $this->get_rating( $item[ 'author_rating' ] );
 				$textual_rating = $rating_data[ 0 ] . '/' . $rating_data[ 1 ];
 				?>
-        <div class="item elementor-repeater-item-<?php echo esc_attr( $item[ '_id' ] ); ?>">
-            <div class="feedback-block-one feedback-block-three">
-                <?php echo wp_get_attachment_image( $item[ 'company_image' ][ 'id' ], 'full', '',
+                <div class="item elementor-repeater-item-<?php echo esc_attr( $item[ '_id' ] ); ?>">
+                    <div class="feedback-block-one feedback-block-three">
+						<?php echo wp_get_attachment_image( $item[ 'company_image' ][ 'id' ], 'full', '',
 							[ 'class' => 'quote-icon' ] ) ?>
-                <?php
+						<?php
 						if ( 'star_fontawesome' === $settings[ 'star_style' ] ) {
 							if ( 'outline' === $settings[ 'unmarked_star_style' ] ) {
 								$icon = '&#xE933;';
@@ -35,40 +35,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 						] );
 
 						$schema_rating = '<span itemprop="ratingValue" class="elementor-screen-only">' . $textual_rating . '</span>';
-						$stars_element = '<div ' . $this->get_render_attribute_string( 'icon_wrapper' ) . '>' . $this->render_stars( $icon,
-								$item[ 'author_rating' ] ) . ' ' . $schema_rating . '</div>';
+						$stars_element = '<div ' . $this->get_render_attribute_string( 'icon_wrapper' ) . '>' . $this->render_stars( $icon, $item[ 'author_rating' ] ) . ' ' . $schema_rating . '</div>';
 						?>
-                <div class="review">
-                    <?php if ( ! \Elementor\Utils::is_empty( $item[ 'author_rating_title' ] ) ) : ?>
-                    <div class="text-md fw-500"><?php echo esc_html( $item[ 'author_rating_title' ] ); ?></div>
-                    <?php endif; ?>
+                        <div class="review">
+							<?php if ( ! \Elementor\Utils::is_empty( $item[ 'author_rating_title' ] ) ) : ?>
+                                <div class="text-md fw-500"><?php echo esc_html( $item[ 'author_rating_title' ] ); ?></div>
+							<?php endif; ?>
 
-                    <?php if ( $testimonial_ratting_icon == 'yes' ) : ?>
-                    <?php echo $stars_element ?>
-                    <?php endif; ?>
-                </div>
-                <?php
+							<?php if ( $testimonial_ratting_icon == 'yes' ) : ?>
+								<?php echo $stars_element ?>
+							<?php endif; ?>
+                        </div>
+						<?php
 						if ( ! empty( $item[ 'review_content' ] ) ) { ?>
-                <h3><?php echo esc_html( $item[ 'review_content' ] ) ?>
-                </h3>
-                <?php
+                            <h3><?php echo esc_html( $item[ 'review_content' ] ) ?></h3>
+							<?php
 						}
 						if ( ! empty( $item[ 'author_name' ] ) ) { ?>
-                <div class="block-footer ezd-d-flex ezd-align-items-center ezd-justify-content-between pt-35 lg-pt-10">
-                    <div class="ezd-d-flex ezd-align-items-center">
-                        <div class="name">
-                            <?php echo esc_html( $item[ 'author_name' ] ); ?>
-                            <span><?php echo esc_html( $item[ 'author_position' ] ); ?></span>
-                        </div>
-                    </div>
-                    <?php echo wp_get_attachment_image($item['author_image']['id'], 'full', '', ['class' => 'author-img ezd-rounded-circle']) ?>
-                </div>
-                <?php
+                            <div class="block-footer ezd-d-flex ezd-align-items-center ezd-justify-content-between pt-35 lg-pt-10">
+                                <div class="ezd-d-flex ezd-align-items-center">
+                                    <div class="name">
+										<?php echo esc_html( $item[ 'author_name' ] ); ?>
+                                        <span><?php echo esc_html( $item[ 'author_position' ] ); ?></span>
+                                    </div>
+                                </div>
+								<?php echo wp_get_attachment_image( $item[ 'author_image' ][ 'id' ], 'full', '', [ 'class' => 'author-img ezd-rounded-circle' ] ) ?>
+                            </div>
+							<?php
 						}
 						?>
-            </div>
-        </div>
-        <?php
+                    </div>
+                </div>
+				<?php
 			}
 		}
 		?>
--- widgets/templates/testimonials/testimonial-8.php
+++ widgets/templates/testimonials/testimonial-8.php
@@ -1,99 +1,105 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
 <div id="feedBack_carousel" class="carousel slide">
     <div class="feedback_slider">
         <div class="carousel-inner ezd-text-center">
-            <?php if(!empty($testimonials8)): 
-                foreach ($testimonials8 as $index => $item ) : 
-                            $i = $index + 1;
-							$active = $i == 1 ? 'active' : '';
-                        ?>
-            <div class="item carousel-item <?php echo esc_attr($active)?>">
-                <?php
-                        if ( !empty($item['review_content']) ) { ?>
-                <p><?php echo esc_html($item['review_content']) ?>
-                </p>
-                <?php
-                        }
-                        if ( !empty($item['author_name']) ) { ?>
-                <div class="ezd-d-inline-block ezd-position-relative name fw-500 text-lg">
-                    <?php echo esc_html($item['author_name']); ?><span class="fw-normal opacity-50">
-                        <?php echo esc_html($item['author_position']); ?></span>
-                </div>
-                <?php
-                        }
-                    ?>
-            </div>
-            <?php 
-            endforeach;
-            endif;
-        ?>
+			<?php if ( ! empty( $testimonials8 ) ):
+				foreach ( $testimonials8 as $index => $item ) :
+					$i = $index + 1;
+					$active = $i == 1 ? 'active' : '';
+					?>
+                    <div class="item carousel-item <?php echo esc_attr( $active ) ?>">
+						<?php
+						if ( ! empty( $item[ 'review_content' ] ) ) { ?>
+                            <p><?php echo esc_html( $item[ 'review_content' ] ) ?>
+                            </p>
+							<?php
+						}
+						if ( ! empty( $item[ 'author_name' ] ) ) { ?>
+                            <div class="ezd-d-inline-block ezd-position-relative name fw-500 text-lg">
+								<?php echo esc_html( $item[ 'author_name' ] ); ?><span class="fw-normal opacity-50">
+                        <?php echo esc_html( $item[ 'author_position' ] ); ?></span>
+                            </div>
+							<?php
+						}
+						?>
+                    </div>
+				<?php
+				endforeach;
+			endif;
+			?>
         </div>
     </div>
     <button class="carousel-control-prev carousel-btn" type="button" data-target="#feedBack_carousel"
-        data-bs-slide="prev">
+            data-bs-slide="prev">
         <i class="eicon-angle-left"></i>
     </button>
     <button class="carousel-control-next carousel-btn" type="button" data-target="#feedBack_carousel"
-        data-bs-slide="next">
+            data-bs-slide="next">
         <i class="eicon-angle-right"></i>
     </button>
     <div class="carousel-indicators">
-        <?php if(!empty($testimonials8)):
-            foreach ($testimonials8 as $index => $item):
-                $i= $index;
-                $active = $i == 0 ? 'active' : '';
-                $area_selected = $i == 1 ? 'true' : 'false'
-            ?>
-        <button type="button" class="<?php echo esc_attr($active)?>" data-target="#feedBack_carousel"
-            data-slide-to="<?php echo esc_attr($i)?>">
-            <?php echo wp_get_attachment_image( $item['author_image']['id'], ' lazy-img ezd-rounded-circle' ) ?>
-        </button>
-        <?php endforeach; endif; ?>
+		<?php if ( ! empty( $testimonials8 ) ):
+			foreach ( $testimonials8 as $index => $item ):
+				$i = $index;
+				$active = $i == 0 ? 'active' : '';
+				$area_selected = $i == 1 ? 'true' : 'false'
+				?>
+                <button type="button" class="<?php echo esc_attr( $active ) ?>" data-target="#feedBack_carousel"
+                        data-slide-to="<?php echo esc_attr( $i ) ?>">
+					<?php echo wp_get_attachment_image( $item[ 'author_image' ][ 'id' ],
+						' lazy-img ezd-rounded-circle' ) ?>
+                </button>
+			<?php endforeach; endif; ?>
         >>>>>>> development
     </div>
 </div>
 <script>
-(function($) {
+    (function ($) {
 
-    $(document).ready(function() {
-        var carousel = $(".carousel");
-        var items = $(".carousel-item");
-        var indicators = $(".carousel-indicators button");
-        var itemCount = items.length;
-        var currentIndex = 0;
+        $(document).ready(function () {
+            var carousel = $(".carousel");
+            var items = $(".carousel-item");
+            var indicators = $(".carousel-indicators button");
+            var itemCount = items.length;
+            var currentIndex = 0;
 
-        function showSlide(index) {
-            if (index < 0) {
-                index = itemCount - 1;
-            } else if (index >= itemCount) {
-                index = 0;
+            function showSlide(index) {
+                if (index < 0) {
+                    index = itemCount - 1;
+                } else if (index >= itemCount) {
+                    index = 0;
+                }
+                var translateX = -index * 100 + "%";
+                $(".carousel-inner").css("transform", "translateX(" + translateX + ")");
+                currentIndex = index;
+
+                // Update the active indicator
+                indicators.removeClass("active");
+                indicators.eq(index).addClass("active");
+                items.removeClass("active");
+                items.eq(index).addClass("active");
             }
-            var translateX = -index * 100 + "%";
-            $(".carousel-inner").css("transform", "translateX(" + translateX + ")");
-            currentIndex = index;
 
-            // Update the active indicator
-            indicators.removeClass("active");
-            indicators.eq(index).addClass("active");
-            items.removeClass("active");
-            items.eq(index).addClass("active");
-        }
+            indicators.click(function () {
+                var index = $(this).data("slide-to");
+                showSlide(index);
+            });
 
-        indicators.click(function() {
-            var index = $(this).data("slide-to");
-            showSlide(index);
+            $(".carousel-control-prev").click(function (e) {
+                e.preventDefault();
+                showSlide(currentIndex - 1);
+            });
+
+            $(".carousel-control-next").click(function (e) {
+                e.preventDefault();
+                showSlide(currentIndex + 1);
+            });
         });
 
-        $(".carousel-control-prev").click(function(e) {
-            e.preventDefault();
-            showSlide(currentIndex - 1);
-        });
 
-        $(".carousel-control-next").click(function(e) {
-            e.preventDefault();
-            showSlide(currentIndex + 1);
-        });
-    });
-
-
-})(jQuery);
+    })(jQuery);
 </script>
