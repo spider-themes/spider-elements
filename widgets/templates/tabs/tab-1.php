c8157ff3b472831da23d1a289bddc8f3167a5031
Rony015 <53683822+Rony015@users.noreply.github.com>
2023-11-14 15:07:40 +0600

    tab code fix

--- widgets/templates/tabs/tab-1.php
+++ widgets/templates/tabs/tab-1.php
@@ -6,8 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="wrapper tab_shortcode">
     <div class="sticky_tab_item tabs_sliders <?php echo esc_attr( $navigation_arrow_class . $sticky_tab_class ); ?>">
         <span class="scroller-btn left"><i class="arrow_carrot-left"></i></span>
-        <ul class="nav nav-tabs d-flex slide_nav_tabs" role="tablist">
-			<?php
+        <ul class="nav nav-tabs ezd-d-flex slide_nav_tabs ezd-tab-menu">
+            <?php
 			$i                         = 0.2;
 			foreach ( $tabs as $index => $item ) :
 				$tab_count = $index + 1;
@@ -24,16 +24,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 					'aria-selected'  => $selected,
 				] );
 				?>
-                <li class="nav-item wow fadeInUp" data-wow-delay="<?php echo esc_attr( $i ); ?>s">
-                    <button <?php echo $this->get_render_attribute_string( $tab_title_setting_key ); ?>>
-						<?php if ( $is_auto_numb == 'yes' ) : ?>
-                            <span class="numb"><?php echo esc_html( $tab_count ) ?></span>
-						<?php endif; ?>
-						<?php \Elementor\Icons_Manager::render_icon( $item['icon'], [ 'aria-hidden' => 'true' ] ); ?>
-						<?php echo esc_html( $item['tab_title'] ); ?>
-                    </button>
-                </li>
-				<?php
+            <li class="nav-item wow fadeInUp" data-wow-delay="<?php echo esc_attr( $i ); ?>s">
+                <button <?php echo $this->get_render_attribute_string( $tab_title_setting_key ); ?>>
+                    <?php if ( $is_auto_numb == 'yes' ) : ?>
+                    <span class="numb"><?php echo esc_html( $tab_count ) ?></span>
+                    <?php endif; ?>
+                    <?php \Elementor\Icons_Manager::render_icon( $item['icon'], [ 'aria-hidden' => 'true' ] ); ?>
+                    <?php echo esc_html( $item['tab_title'] ); ?>
+                </button>
+            </li>
+            <?php
 				$i = $i + 0.2;
 			endforeach;
 			?>
@@ -41,20 +41,20 @@ if ( ! defined( 'ABSPATH' ) ) {
         <span class="scroller-btn right" id="right"><i class="arrow_carrot-right"></i></span>
     </div>
     <div class="tab-content">
-		<?php
+        <?php
 		foreach ( $tabs as $index => $item ) {
 			$tab_count               = $index + 1;
 			$active                  = $tab_count == 1 ? 'show active' : '';
 			$tab_content_setting_key = $this->get_repeater_setting_key( 'tab_content', 'tabs', $index );
 			$this->add_render_attribute( $tab_content_setting_key, [
-				'class'           => [ 'tab-pane p-0 tab_style', 'fade', $active ],
+				'class'           => [ 'tab-pane p-0 tab_style ezd-tab-box', 'fade', $active ],
 				'id'              => 'tab-content-' . $id_int . $tab_count,
 				'aria-labelledby' => 'tab-' . $id_int . $tab_count,
 				'role'            => 'tabpanel',
 			] );
 			?>
-            <div <?php echo $this->get_render_attribute_string( $tab_content_setting_key ); ?>>
-				<?php
+        <div <?php echo $this->get_render_attribute_string( $tab_content_setting_key ); ?>>
+            <?php
 				if ( 'content' == $item['tabs_content_type'] ) {
 					echo do_shortcode( $item['tab_content'] );
 				} elseif ( 'template' == $item['tabs_content_type'] ) {
@@ -63,18 +63,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 					}
 				}
 				?>
-            </div>
-			<?php
+        </div>
+        <?php
 		}
 
 		if ( $is_navigation_arrow == 'yes' ) { ?>
-            <button class="btn btn-info btn-lg previous"><i class="arrow_carrot-left"></i></button>
-            <button class="btn btn-info btn-lg next"><i class="arrow_carrot-right"></i></button>
-			<?php
+        <button class="btn btn-info btn-lg previous"><i class="arrow_carrot-left"></i></button>
+        <button class="btn btn-info btn-lg next"><i class="arrow_carrot-right"></i></button>
+        <?php
 		}
 		?>
     </div>
 </div>
-
-
-
--- widgets/templates/testimonials/testimonial-8.php
+++ widgets/templates/testimonials/testimonial-8.php
@@ -6,100 +6,99 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="feedBack_carousel" class="carousel slide">
     <div class="feedback_slider">
         <div class="carousel-inner ezd-text-center">
-			<?php if ( ! empty( $testimonials8 ) ):
+            <?php if ( ! empty( $testimonials8 ) ):
 				foreach ( $testimonials8 as $index => $item ) :
 					$i = $index + 1;
 					$active = $i == 1 ? 'active' : '';
 					?>
-                    <div class="item carousel-item <?php echo esc_attr( $active ) ?>">
-						<?php
+            <div class="item carousel-item <?php echo esc_attr( $active ) ?>">
+                <?php
 						if ( ! empty( $item[ 'review_content' ] ) ) { ?>
-                            <p><?php echo esc_html( $item[ 'review_content' ] ) ?>
-                            </p>
-							<?php
+                <p><?php echo esc_html( $item[ 'review_content' ] ) ?>
+                </p>
+                <?php
 						}
 						if ( ! empty( $item[ 'author_name' ] ) ) { ?>
-                            <div class="ezd-d-inline-block ezd-position-relative name fw-500 text-lg">
-								<?php echo esc_html( $item[ 'author_name' ] ); ?><span class="fw-normal opacity-50">
+                <div class="ezd-d-inline-block ezd-position-relative name fw-500 text-lg">
+                    <?php echo esc_html( $item[ 'author_name' ] ); ?><span class="fw-normal opacity-50">
                         <?php echo esc_html( $item[ 'author_position' ] ); ?></span>
-                            </div>
-							<?php
+                </div>
+                <?php
 						}
 						?>
-                    </div>
-				<?php
+            </div>
+            <?php
 				endforeach;
 			endif;
 			?>
         </div>
     </div>
     <button class="carousel-control-prev carousel-btn" type="button" data-target="#feedBack_carousel"
-            data-bs-slide="prev">
+        data-bs-slide="prev">
         <i class="eicon-angle-left"></i>
     </button>
     <button class="carousel-control-next carousel-btn" type="button" data-target="#feedBack_carousel"
-            data-bs-slide="next">
+        data-bs-slide="next">
         <i class="eicon-angle-right"></i>
     </button>
     <div class="carousel-indicators">
-		<?php if ( ! empty( $testimonials8 ) ):
+        <?php if ( ! empty( $testimonials8 ) ):
 			foreach ( $testimonials8 as $index => $item ):
 				$i = $index;
 				$active = $i == 0 ? 'active' : '';
 				$area_selected = $i == 1 ? 'true' : 'false'
 				?>
-                <button type="button" class="<?php echo esc_attr( $active ) ?>" data-target="#feedBack_carousel"
-                        data-slide-to="<?php echo esc_attr( $i ) ?>">
-					<?php echo wp_get_attachment_image( $item[ 'author_image' ][ 'id' ],
+        <button type="button" class="<?php echo esc_attr( $active ) ?>" data-target="#feedBack_carousel"
+            data-slide-to="<?php echo esc_attr( $i ) ?>">
+            <?php echo wp_get_attachment_image( $item[ 'author_image' ][ 'id' ],
 						' lazy-img ezd-rounded-circle' ) ?>
-                </button>
-			<?php endforeach; endif; ?>
-        >>>>>>> development
+        </button>
+        <?php endforeach; endif; ?>
     </div>
 </div>
 <script>
-    (function ($) {
+(function($) {
 
-        $(document).ready(function () {
-            var carousel = $(".carousel");
-            var items = $(".carousel-item");
-            var indicators = $(".carousel-indicators button");
-            var itemCount = items.length;
-            var currentIndex = 0;
+    $(document).ready(function() {
+        var carousel = $(".carousel");
+        var items = $(".carousel-item");
+        var indicators = $(".carousel-indicators button");
+        var itemCount = items.length;
+        var currentIndex = 0;
 
-            function showSlide(index) {
-                if (index < 0) {
-                    index = itemCount - 1;
-                } else if (index >= itemCount) {
-                    index = 0;
-                }
-                var translateX = -index * 100 + "%";
-                $(".carousel-inner").css("transform", "translateX(" + translateX + ")");
-                currentIndex = index;
-
-                // Update the active indicator
-                indicators.removeClass("active");
-                indicators.eq(index).addClass("active");
-                items.removeClass("active");
-                items.eq(index).addClass("active");
+        function showSlide(index) {
+            if (index < 0) {
+                index = itemCount - 1;
+            } else if (index >= itemCount) {
+                index = 0;
             }
+            var translateX = -index * 100 + "%";
+            $(".carousel-inner").css("transform", "translateX(" + translateX + ")");
+            currentIndex = index;
 
-            indicators.click(function () {
-                var index = $(this).data("slide-to");
-                showSlide(index);
-            });
+            // Update the active indicator
+            indicators.removeClass("active");
+            indicators.eq(index).addClass("active");
+            items.removeClass("active");
+            items.eq(index).addClass("active");
+        }
 
-            $(".carousel-control-prev").click(function (e) {
-                e.preventDefault();
-                showSlide(currentIndex - 1);
-            });
-
-            $(".carousel-control-next").click(function (e) {
-                e.preventDefault();
-                showSlide(currentIndex + 1);
-            });
+        indicators.click(function() {
+            var index = $(this).data("slide-to");
+            showSlide(index);
         });
 
+        $(".carousel-control-prev").click(function(e) {
+            e.preventDefault();
+            showSlide(currentIndex - 1);
+        });
 
-    })(jQuery);
+        $(".carousel-control-next").click(function(e) {
+            e.preventDefault();
+            showSlide(currentIndex + 1);
+        });
+    });
+
+
+})(jQuery);
 </script>
